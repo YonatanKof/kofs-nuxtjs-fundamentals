a4666847725ab5096f801d2dd0a1fdc526f9c6e0
Yonatan Kof <kof@yonatankof.com>
2023-05-23 09:42:30 +0300

    Add scss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	css: ['~/assets/style.scss'],
+	css: ['~/assets/styles/style.scss'],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: `
+						@use "@/assets/styles/_mixins.scss" as *; 
+						@use "@/assets/styles/_try.scss" as *;
+					`,	
+				},
+			},
+		},
+	},
 });
