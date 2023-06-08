da0adb2e6a650d63f2d691ec4e6e904dffe628b2
Yonatan Kof <kof@yonatankof.com>
2023-06-08 08:01:59 +0300

    Add nuxt-content and code highlighting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+	modules: ['@nuxt/content'],
+	content: {
+		highlight: {
+			theme: 'github-light',
+		},
+	},
 	css: ['~/assets/styles/style.scss'],
 	vite: {
 		css: {
@@ -8,7 +14,7 @@ export default defineNuxtConfig({
 					additionalData: `
 						@use "@/assets/styles/_mixins.scss" as *; 
 						@use "@/assets/styles/_try.scss" as *;
-					`,	
+					`,
 				},
 			},
 		},
