9b485d8e4c35e8a81fb7891791dca13e654f1257
Yonatan Kof <kof@yonatankof.com>
2023-06-08 08:10:02 +0300

    Querying the content folder

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,20 @@
 <script setup>
-import MyComp from '../components/MyComp.vue';
+const { data: equalQuery } = await useAsyncData('equal', () => {
+	return queryContent('post').where({ color: 'red' }).find();
+});
+
 </script>
 <template>
 	<div>
-        <h1>Index</h1>
+		<h1>Index</h1>
+		<h2>Querying the content folder</h2>
+		<ul v-if="equalQuery">
+			<li v-for="color in equalQuery" :key="color.id">Color is {{ color.color }} and it feels {{ color.feeling }}</li>
+		</ul>
+		<h2>Just a simple component</h2>
 		<MyComp>
-            <code>You slot!</code><br/>
-            <h3>I love you</h3>
-        </MyComp>
+			<code>You slot!</code><br />
+			<h3>I love you</h3>
+		</MyComp>
 	</div>
 </template>
