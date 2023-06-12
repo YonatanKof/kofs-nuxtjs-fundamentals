fd47e1425c84a29e4bd8c9a4488e284b78f06cc5
Yonatan Kof <kof@yonatankof.com>
2023-06-12 09:01:50 +0300

    More meta tags

--- app.vue
+++ app.vue
@@ -2,10 +2,6 @@
 useHead({
 	title: 'Kof tov',
 	meta: [{ name: 'description', content: 'My amazing site.' }],
-	bodyAttrs: {
-		class: 'test',
-	},
-	script: [{ innerHTML: "console.log('Hello world')" }],
 });
 </script>
 
---
+++ pages/kof/kof-tov.vue
@@ -0,0 +1,14 @@
+<script setup>
+const title = ref('Welcome to my world');
+</script>
+
+<template>
+	<div>
+		<Head>
+			<Title>{{ title }}</Title>
+			<Meta name="description" :content="title" />
+			<Style type="text/css" children="body { background-color: green; }" />
+		</Head>
+		<h1>Kof tov</h1>
+	</div>
+</template>
