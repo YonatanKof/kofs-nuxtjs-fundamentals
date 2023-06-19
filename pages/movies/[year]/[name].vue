8174c2f5d7f1cd14fb1f8abb4b0206e2caeb9afc
Yonatan Kof <kof@yonatankof.com>
2023-06-19 07:28:55 +0300

    Dynamic route and VVueUse change case

---
+++ pages/movies/[year]/[name].vue
@@ -0,0 +1,23 @@
+<script setup>
+import { useChangeCase } from '@vueuse/integrations/useChangeCase';
+const route = useRoute();
+
+const goodLookingName = useChangeCase(route.params.name, 'capitalCase');
+const mes = computed(() => `${goodLookingName.value} is form year ${route.params.year} `);
+</script>
+
+<template>
+	<div>
+		<h1>
+			{{ mes }}
+		</h1>
+		<ul>
+			<li>
+				Movie name: <strong>{{ goodLookingName }}</strong>
+			</li>
+			<li>
+				Year of release: <strong>{{ route.params.year }}</strong>
+			</li>
+		</ul>
+	</div>
+</template>
