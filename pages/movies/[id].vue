f276913083d347c0f5196de2c2da22f7a3d346ea
Yonatan Kof <kof@yonatankof.com>
2023-07-08 15:37:15 +0300

    Call movie data with useAsyncData

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,7 +1,23 @@
-<script lang="ts" setup></script>
+<script setup>
+const route = useRoute();
+const { data } = await useAsyncData(
+	`/movies/${route.params.id}`,
+	() => {
+		return $fetch(`http://www.omdbapi.com/?apikey=fab7e09d&i=${route.params.id}`);
+	},
+	{ pick: ['Title', 'Poster', 'Director', 'Released', 'Plot'] }
+);
+</script>
 
 <template>
-	<div>Page: {{ $route.params.id }}</div>
+	<main>
+		<h2>{{ data.Title }}</h2>
+		<img :src="data.Poster" :alt="`Cover poster for the movie ${data.Title}`" />
+		<h3>A movie by {{ data.Director }}, released on {{ data.Released }}</h3>
+		<p>{{ data.Plot }}</p>
+		<!-- This shows all the available data -->
+		<pre>{{ data }}</pre>
+	</main>
 </template>
 
 <style scoped></style>
