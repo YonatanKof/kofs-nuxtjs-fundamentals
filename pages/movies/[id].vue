eaeb21a83eba7193aacafb8a46a39731c4cd7b01
Yonatan Kof <kof@yonatankof.com>
2023-07-08 15:50:28 +0300

    Use useFetch instead of useAsyncData and $fetch

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -1,22 +1,19 @@
 <script setup>
 const route = useRoute();
-const { data } = await useAsyncData(
-	`/movies/${route.params.id}`,
-	() => {
-		return $fetch(`http://www.omdbapi.com/?apikey=fab7e09d&i=${route.params.id}`);
-	},
-	{ pick: ['Title', 'Poster', 'Director', 'Released', 'Plot'] }
-);
+const { data } = await useFetch(`http://www.omdbapi.com/?apikey=fab7e09d&i=${route.params.id}`, {
+	pick: ['Title', 'Poster', 'Director', 'Released', 'Plot', 'Actors'],
+});
 </script>
 
 <template>
 	<main>
-		<h2>{{ data.Title }}</h2>
+		<h1>{{ data.Title }}</h1>
 		<img :src="data.Poster" :alt="`Cover poster for the movie ${data.Title}`" />
 		<h3>A movie by {{ data.Director }}, released on {{ data.Released }}</h3>
+		<h4>Staring: {{ data.Actors }}</h4>
 		<p>{{ data.Plot }}</p>
 		<!-- This shows all the available data -->
-		<pre>{{ data }}</pre>
+		<!-- <pre>{{ data }}</pre> -->
 	</main>
 </template>
 
