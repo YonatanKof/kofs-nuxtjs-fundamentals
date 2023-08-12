1389d65963a1cc72e8505ede86dc0b69bce8c169
Yonatan Kof <kof@yonatankof.com>
2023-08-12 23:03:08 +0300

    Change apiSecret name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,7 @@ export default defineNuxtConfig({
 		// Private keys are only available on the server
 		// Public keys that are exposed to the client
 		public: {
-			apiSecret: process.env.NUXT_API_KEY,
+			apiSecret: process.env.NUXT_OMDBAPI_KEY,
 			siteOwnerName: 'Yonatan Ben Knaan',
 			missingImg: '/missing.svg',
 		},
