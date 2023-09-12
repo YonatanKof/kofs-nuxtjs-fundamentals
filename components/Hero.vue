5b578360204d54713efb560c036b3301e06520ef
Yonatan Kof <kof@yonatankof.com>
2023-09-12 22:45:14 +0300

    (Don't) add hero

---
+++ components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+	<Section id="blog">
+		<div>
+			<ol itemscope itemtype="https://schema.org/BreadcrumbList">
+				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+					<nuxt-link itemprop="item" :to="{ name: 'index' }"><span itemprop="name">Home</span></nuxt-link>
+					<meta itemprop="position" content="1" />
+				</li>
+				<span>/</span>
+				<li itemprop="itemListElement" itemscope itemtype="https://schema.org/ListItem">
+					<span itemprop="name">Blog</span>
+					<meta itemprop="position" content="2" />
+				</li>
+			</ol>
+		</div>
+		<h1>Blog</h1>
+		<p>This is a Kof Tov website</p>
+	</Section>
+</template>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,6 @@
 <template>
 	<main>
+		<!-- <Hero /> -->
 		<ContentDoc />
 	</main>
 </template>
